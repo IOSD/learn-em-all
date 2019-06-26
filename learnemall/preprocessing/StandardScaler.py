e70df51c7627a9aea509e8e3f4f14c76feaa9140
Ekansh Gupta <33174166+eKuG@users.noreply.github.com>
2019-06-27 04:43:10 +0530

    Implemented Standard Scaling

---
+++ learnemall/preprocessing/StandardScaler.py
@@ -0,0 +1,23 @@
+#importing libraries
+import numpy as np
+
+  
+class StandardScaler:
+
+    def __init__(self,X):
+        self.mean=np.mean(X,axis=0)
+        self.std=np.std(X,axis=0)
+
+    def scale(self,dat):
+        z = np.zeros(np.shape(dat),dtype=float)
+        for i in range(np.shape(dat)[-1]):
+            z[:,i] = (dat[:,i]-(np.mean(dat,axis=0)[i]))/(np.std(dat,axis=0)[i])
+
+        return z
+
+    def revert(self,dat):
+        z = np.zeros(np.shape(dat),dtype=float)
+        for i in range(np.shape(dat)[-1]):
+            z[:,i] = ((dat[:,i]*(self.std[i]))+self.mean[i])
+
+        return z
